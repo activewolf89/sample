084aa80fdece14d674f7fc007e1ee11923844436
Reed Denny <reeddenny@gmail.com>
2022-05-29 18:41:53 -0700

    changes for vs2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. v2222
+          Edit <code>src/App.js</code> and save to reload. v2222 try again
         </p>
         <a
           className="App-link"
